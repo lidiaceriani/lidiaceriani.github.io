a4aef3223afbda875d137d11620ac4a8d3788150
lidiaceriani <82227829+lidiaceriani@users.noreply.github.com>
2025-12-18 08:34:34 +0000

    Deploying to gh-pages from @ lidiaceriani/lidiaceriani.github.io@c7c74628eae21e8bb64d4e52b8c7c23a8e37fefc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,16 +398,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-book-discussion-non-è-giusta-l-italia-delle-disuguaglianze-laterza-2025-location-library-walter-bigiavi-via-delle-belle-arti-33-40126-bologna",
+          title: 'Book discussion “Non è giusta. L’Italia delle disuguaglianze”, Laterza, 2025 Location: Library “Walter...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
